67a28e581cddb4e4de46a58854001424bafcd9cd
David Loesch <davidloesch1@gmail.com>
2020-03-14 13:20:04 -0700

    everything seems to be working starting on delete button

--- cal_app/forms.py
+++ cal_app/forms.py
@@ -1,4 +1,6 @@
-from django.forms import ModelForm, DateInput
+from django.forms import ModelForm, DateTimeInput, DateTimeField, DateInput
+from django.contrib.admin import widgets                                       
+
 from cal_app.models import Event
 
 class EventForm(ModelForm):
@@ -8,9 +10,10 @@ class EventForm(ModelForm):
             'start_time': DateInput(attrs={'type': 'datetime-local'}, format='%Y-%m-%dT%H:%M'),
             'end_time': DateInput(attrs={'type': 'datetime-local'}, format=('%Y-%m-%dT%H:%M')),
         }
+
         fields = '__all__'
 
     def __init__(self, *args, **kargs):
         super(EventForm, self).__init__(*args, **kargs)
         self.fields['start_time'].input_formats = ('%Y-%m-%dT%H:%M',)
-        self.fields['start_time'].input_formats = ('%Y-%m-%dT%H:%M',)
+        self.fields['end_time'].input_formats = ('%Y-%m-%dT%H:%M',)
--- cal_django/settings.py
+++ cal_django/settings.py
@@ -124,5 +124,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static').replace('\\', '/')
 STATICFILES_DIRS = ('static',)
