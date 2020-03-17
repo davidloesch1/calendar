5fda4d96a5e36deeff622aef1ebb193d85071c34
David Loesch <davidloesch1@gmail.com>
2020-03-16 19:16:42 -0700

    finalllllly got the signup link to work

--- cal_app/urls.py
+++ cal_app/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import url
-from . import views
+from django.urls import path
+from . import views as core_views
 
 app_name = 'cal_app'
 urlpatterns = [
-    url(r'^index/$', views.index, name='index'),
-    url(r'^calendar/$', views.CalendarView.as_view(), name='calendar'),
-    url(r'^signup/$', views.signup, name='signup'),
-    url(r'^event/new/$', views.new, name='event_new'),
-    url(r'^event/edit/(?P<event_id>\d+)/$', views.edit, name='event_edit'),
-    url(r'^event/delete/(?P<event_id>\d+)/$', views.delete, name='delete'),
+    url(r'^index/$', core_views.index, name='index'),
+    url(r'^calendar/$', core_views.CalendarView.as_view(), name='calendar'),
+    url(r'^event/new/$', core_views.new, name='event_new'),
+    url(r'^event/edit/(?P<event_id>\d+)/$', core_views.edit, name='event_edit'),
+    url(r'^event/delete/(?P<event_id>\d+)/$', core_views.delete, name='delete'),
+    url(r'^signup/$', core_views.signup, name='signup'),
 ]
