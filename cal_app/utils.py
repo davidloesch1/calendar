156a6298283dc42a73f2b39dfa178f548e1a4553
David Loesch <davidloesch1@gmail.com>
2020-03-17 12:44:23 -0700

    change

--- cal_app/utils.py
+++ cal_app/utils.py
@@ -28,7 +28,6 @@ class Calendar(HTMLCalendar):
     def formatmonth(self, withyear=True):
 
         events = Event.objects.filter(start_time__year=self.year, start_time__month=self.month, owner_id=self.owner_id)
-        print(events)
         cal = f'<table border="0" cellpadding="0" cellspacing="0" class="calendar">\n'
         cal += f'{self.formatmonthname(self.year, self.month, withyear=withyear)}\n'
         cal += f'{self.formatweekheader()}\n'
